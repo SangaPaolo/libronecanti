3622841be6afd5bd489ef51159bdda1f81d0bb89
Paolo Sangalli <paolo.sangalli1@studenti.unimi.it>
2025-06-13 11:14:59 +0200

    searchbar layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,13 +9,15 @@ import { SafeAreaView, View } from "react-native";
 export default function Index() {
 	return (
 		<SafeAreaView className="flex flex-col items-center justify-center size-full bg-primary-dark">
-			<SearchBar />
-			<LastSeen />
-			<View className="flex flex-row items-center justify-between">
-				<IndexPreview />
-				<View className="flex flex-col items-center justify-center">
-					<PreferitiPreview />
-					<TopMoth />
+			<View className="size-full px-7 pt-5">
+				<SearchBar />
+				<LastSeen />
+				<View className="flex flex-row items-center justify-between">
+					<IndexPreview />
+					<View className="flex flex-col items-center justify-center">
+						<PreferitiPreview />
+						<TopMoth />
+					</View>
 				</View>
 			</View>
 		</SafeAreaView>
--- components/HomePage/SearchBar.tsx
+++ components/HomePage/SearchBar.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, ImageSourcePropType, TextInput, View } from "react-native";
 
 const SearchBar = () => {
-  return (
-    <View>
-      <Text>SearchBar</Text>
-    </View>
-  )
-}
+	return (
+		<View className="flex bg-white w-full h-14 justify-center items-center mt-4 rounded-[10px] shadow-sm">
+			<View className="flex flex-row items-center justify-between w-full px-3">
+				<TextInput
+					className="flex-1 text-xl text-black h-full py-0 justify-center"
+					placeholder="Cerca un brano..."
+					placeholderTextColor="#897C80"
+				/>
+				<Image
+					source={icons.searchLens as ImageSourcePropType}
+					className="w-5 h-5 ml-2"
+					resizeMode="contain"
+				/>
+			</View>
+		</View>
+	);
+};
 
-export default SearchBar
+export default SearchBar;
