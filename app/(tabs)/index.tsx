c8af5b702cffab6528d99e2153a018b2673cce31
Paolo Sangalli <paolo.sangalli1@studenti.unimi.it>
2025-06-13 10:17:39 +0200

    inserimento componenti

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,23 @@
+import IndexPreview from "@/components/HomePage/IndexPreview";
+import LastSeen from "@/components/HomePage/LastSeen";
+import PreferitiPreview from "@/components/HomePage/PreferitiPreview";
+import SearchBar from "@/components/HomePage/SearchBar";
+import TopMoth from "@/components/HomePage/TopMoth";
 import "@/global.css";
-import { Text, View } from "react-native";
+import { SafeAreaView, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold">Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+	return (
+		<SafeAreaView className="flex flex-col items-center justify-center size-full bg-primary-dark">
+			<SearchBar />
+			<LastSeen />
+			<View className="flex flex-row items-center justify-between">
+				<IndexPreview />
+				<View className="flex flex-col items-center justify-center">
+					<PreferitiPreview />
+					<TopMoth />
+				</View>
+			</View>
+		</SafeAreaView>
+	);
 }
