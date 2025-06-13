0e17892b3f2e85750aa33257bff2ed4f6959c92f
Paolo Sangalli <paolo.sangalli1@studenti.unimi.it>
2025-06-13 11:59:38 +0200

    searchbar tmp prima delle query

--- components/HomePage/SearchBar.tsx
+++ components/HomePage/SearchBar.tsx
@@ -1,22 +1,42 @@
 import { icons } from "@/constants/icons";
-import React from "react";
-import { Image, ImageSourcePropType, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Image,
+  ImageSourcePropType,
+  TextInput,
+  View
+} from "react-native";
+import SearchBarButton from "../SearchBarButton";
 
 const SearchBar = () => {
+	const [searchQuery, setSearchQuery] = useState("");
+
 	return (
-		<View className="flex bg-white w-full h-14 justify-center items-center mt-4 rounded-[10px] shadow-sm">
-			<View className="flex flex-row items-center justify-between w-full px-3">
-				<TextInput
-					className="flex-1 text-xl text-black h-full py-0 justify-center"
-					placeholder="Cerca un brano..."
-					placeholderTextColor="#897C80"
-				/>
-				<Image
-					source={icons.searchLens as ImageSourcePropType}
-					className="w-5 h-5 ml-2"
-					resizeMode="contain"
-				/>
+		<View>
+			<View className="flex bg-white w-full h-14 justify-center items-center mt-4 rounded-[10px] shadow-sm">
+				<View className="flex flex-row items-center justify-between w-full px-3">
+					<TextInput
+						className="flex-1 text-xl text-defaultText h-full justify-center"
+						placeholder="Cerca un brano..."
+						placeholderTextColor="#897C80"
+						onChangeText={(text) => setSearchQuery(text)}
+						value={searchQuery}
+            autoCorrect={false}
+            spellCheck={false}
+            selectionColor={"#897C80"}
+					/>
+					<Image
+						source={icons.searchLens as ImageSourcePropType}
+						className="w-5 h-5 ml-2"
+						resizeMode="contain"
+					/>
+				</View>
 			</View>
+			{searchQuery.length >= 3 && (
+				<View className="flex flex-col items-center justify-center bg-white rounded-[10px] h-full max-h-[60%] mt-4 p-2 shadow-sm">
+					<SearchBarButton />
+				</View>
+			)}
 		</View>
 	);
 };
---
+++ components/SearchBarButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const SearchBarButton = () => {
+  return (
+    <View className='size-full bg-secondary rounded-[6px] flex items-center justify-center'>
+      <Text>SearchBarButton</Text>
+    </View>
+  )
+}
+
+export default SearchBarButton
